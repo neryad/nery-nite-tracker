acff5774b622bae23721234953e12d797e3a4eb5
Neryad <dayern.gomez@hotmail.com>
2025-04-21 21:51:25 -0400

    Add new asset images for iOS and Windows 11, and configure PWA support in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+import iconsJson from "./icons.json";
+// const manifestIcons = iconsJson.icons.map((icon) => ({
+//   src: `/src/assets/${icon.src}`,
+//   sizes: icon.sizes,
+//   type: "image/png",
+// }));
 // https://vite.dev/config/
+
+const manifestIcons = [
+  {
+    src: "src/assets/android/android-launchericon-192-192.png",
+    sizes: "192x192",
+    type: "image/png",
+  },
+  {
+    src: "src/assets/android/android-launchericon-512-512.png",
+    sizes: "512x512",
+    type: "image/png",
+  },
+];
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Nery Nite Tracker",
+        short_name: "Nery Nite",
+        icons: manifestIcons,
+      },
+    }),
+  ],
 });
